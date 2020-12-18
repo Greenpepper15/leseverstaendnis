1255c22a59448b0bc84d0a1b9c040471d187d7a3
Greenpepper15 <64377431+Greenpepper15@users.noreply.github.com>
2020-12-18 19:46:42 +0100

    Update website.js

--- website.js
+++ website.js
@@ -1,15 +1,14 @@
 
 
-function sendEmail(element_id) {
-  var x = document.getElementById(element_id).value;  
+function sendEmail(element,Frage) {
     Email.send({
       Host : "smtp.mailtrap.io",
       Username : "af59f73491b60f",
       Password : "2dbe2d6e5727e0",
       To : 'recipient@example.com',
       From : "sender@example.com",
-      Subject : id + "_" + "Frage1",
-      Body :  createBody(id, x)
+      Subject : id + "_" + Frage,
+      Body :  createBody(id, element)
   }).then(
     message => alert(message)
   );
