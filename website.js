a71465dfab8160ea3b16b342f6692ae19babf100
Greenpepper15 <64377431+Greenpepper15@users.noreply.github.com>
2020-12-18 15:03:08 +0100

    Update website.js

--- website.js
+++ website.js
@@ -1,25 +1,27 @@
 
-function sendEmail() {
-  var x = document.getElementById("myText").value;  
-Email.send({
-    Host : "smtp.mailtrap.io",
-    Username : "af59f73491b60f",
-    Password : "2dbe2d6e5727e0",
-    To : 'recipient@example.com',
-    From : "sender@example.com",
-    Subject : id + "_" + "Frage1",
-    Body :  createBody(id, x)
-}).then(
-  message => alert(message)
-);
-  //changePageRandom();
-  var jo = document.getElementById("goole");
-  jo.parentNode.removeChild(jo);
+
+function sendEmail(element_id) {
+  var x = document.getElementById(element_id).value;  
+    Email.send({
+      Host : "smtp.mailtrap.io",
+      Username : "af59f73491b60f",
+      Password : "2dbe2d6e5727e0",
+      To : 'recipient@example.com',
+      From : "sender@example.com",
+      Subject : id + "_" + "Frage1",
+      Body :  createBody(id, x)
+  }).then(
+    message => alert(message)
+  );
 }
 
-  function removeImage(){
-  var jo = document.getElementById("image");  //Id string von dem zu löschenden element
+function createBody(user_id, data){
+  var toSend = {"user" : user_id, "Frage1": data}; // Bezeichner wichtig fuer Programmzuweisung !!
+  return JSON.stringify(toSend);
+}
+
+  function removeVisability(node, massage){
+  var jo = document.getElementById(node);  //Id string von dem zu löschenden element
   jo.parentNode.removeChild(jo);
-  removeOnce = false;
-  document.getElementById("demo").innerHTML = "EINGABE !!!!"
+  document.getElementById("demo").innerHTML = massage;
 }
